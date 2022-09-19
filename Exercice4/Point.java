d2a45e681c68d137753b4f81fe0283eb60a693e8
Dilien Oeuvrard <74972237+Dili3n@users.noreply.github.com>
2022-09-19 22:11:13 +0200

    Delete Exercice4 directory

--- Exercice4/Point.java
+++
@@ -1,7 +0,0 @@
-public record Point(int x, int y) {
-
-  public double distance(Point orthoPoint) {
-    return Math.sqrt(((orthoPoint.x - 0) * (orthoPoint.x - 0)) + ((orthoPoint.y - 0) * (orthoPoint.y - 0)));
-  }
-}
-
